14fadaebd3204f9ee33365df293cbf5fd17e4c52
Jerquavious <127266586+Jerquavious@users.noreply.github.com>
2024-08-09 18:10:11 -0700

    Create PluginInfo.cs

---
+++ HandHeldTemplate/HandHeldTemplate/PluginInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HandHeldTemplate
+{
+    internal class PluginInfo
+    {
+        public string Name { get; set; } = "NixxQuestClient";
+        public string Version { get; set; } = "1.0.0";
+        public string Author { get; set; } = "54nixx54";
+        public string Description { get; set; } = "Made For Apk Method Games";
+
+        public PluginInfo()
+        {
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Version: {Version}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Description: {Description}");
+        }
+    }
+}
