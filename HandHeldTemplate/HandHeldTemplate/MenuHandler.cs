e60226ea0b783a7df331836978c3c90e9c51be44
Jerquavious <127266586+Jerquavious@users.noreply.github.com>
2024-08-09 17:56:12 -0700

    Update MenuHandler.cs

--- HandHeldTemplate/HandHeldTemplate/MenuHandler.cs
+++ HandHeldTemplate/HandHeldTemplate/MenuHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -29,7 +29,7 @@ namespace Util
                     modNames.Add(modName);
                 }
             }
-            
+
             if (Pages.Count > 1)
             {
                 for (int i = 1; i < Pages.Count; i++)
@@ -110,7 +110,7 @@ namespace Util
                 {
                     currentPage.DisableCurrentPage();
                 }
-                
+
                 Pages[newIndex].EnableCurrentPage();
 
                 return Pages[newIndex];
@@ -132,10 +132,10 @@ namespace Util
         public string modName;
         public Page ParentPage;
         public bool pressDelay = false;
-        public float timeDelay = 0.2f;
+        public float timeDelay = 0.33f;
 
-        public Button (IntPtr ptr) : base(ptr) { }
-        public Button () { }
+        public Button(IntPtr ptr) : base(ptr) { }
+        public Button() { }
 
         public void OnTriggerEnter(Collider other)
         {
@@ -145,14 +145,14 @@ namespace Util
                 if (timeDelay <= 0)
                 {
                     pressDelay = false;
-                    timeDelay = 0.2f;
+                    timeDelay = 0.33f;
                 }
             }
             if (!pressDelay)
             {
                 var gt = GorillaTagger.Instance;
-                gt.StartVibration(false, gt.taggedHapticStrength / 2, gt.taggedHapticDuration);
-                if (modName == "<")
+                gt.StartVibration(false, gt.taggedHapticStrength / 1, gt.taggedHapticDuration);
+                if (modName == "<color=grey>[<] Last Page</color>")
                 {
                     Page previousPage = MenuHandler.GetNextPage(moveForward: false);
                     if (previousPage != null)
@@ -161,7 +161,7 @@ namespace Util
                         state = false;
                     }
                 }
-                else if (modName == ">")
+                else if (modName == "<color=grey>Next Page [>]</color>")
                 {
                     Page nextPage = MenuHandler.GetNextPage();
                     if (nextPage != null)
@@ -175,16 +175,15 @@ namespace Util
                     ParentPage.ToggleModStateAndColor(modName);
                 }
                 pressDelay = true;
-                timeDelay = 0.2f;
+                timeDelay = 0.33f;
             }
         }
     }
 
-    // This is for easier page creation
     class PageInfo
     {
-        public string Name; // The name of the page used in the title text
-        public string[] Mods; // A list of all mods for the page
+        public string Name;
+        public string[] Mods;
 
         public PageInfo(string name, string[] mods)
         {
@@ -195,22 +194,22 @@ namespace Util
 
     class Page
     {
-        public List<GameObject> Pages = new List<GameObject>(); // A list of the page objects for later disabling of inactive pages
-        public List<Button> Buttons = new List<Button>(); // A list of the buttons for later use
-        public List<GameObject> ButtonObjects = new List<GameObject>(); // A list of the button GameObjects
+        public List<GameObject> Pages = new List<GameObject>();
+        public List<Button> Buttons = new List<Button>();
+        public List<GameObject> ButtonObjects = new List<GameObject>();
         public List<string> Mods = new List<string>();
         public Dictionary<string, Button> ModNameToButtonMap = new Dictionary<string, Button>();
-        public Dictionary<Button, bool> ButtonState = new Dictionary<Button, bool>(); // A list of the button states for detecting if the button is enabled
+        public Dictionary<Button, bool> ButtonState = new Dictionary<Button, bool>();
         public GameObject MenuObject = null;
-        public string ForwardButtonName = "<"; // Default forward navigation button name
-        public string BackwardButtonName = ">"; // Default backward navigation button name
+        public string ForwardButtonName = "<color=white>[<] Last Page</color>";
+        public string BackwardButtonName = "<color=white>Next Page [>]</color>";
         public GameObject SharedCanvas = null;
         public GameObject titleObject = null;
         public string Title;
 
         public Page(string title, string[] mods)
         {
-            // Dynamically create the page and the associated buttons with the text
+
             if (MenuObject == null)
             {
                 MenuObject = CreateMenu();
@@ -237,34 +236,32 @@ namespace Util
         private GameObject CreateMenu()
         {
             GameObject menu = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            menu.transform.localScale = new Vector3(0.04f, 0.35f, 0.53f);
-            menu.transform.position = Player.Instance.leftHandTransform.position + new Vector3(0, 0, -1f);
+            menu.transform.localScale = new Vector3(0.1f, 0.3f, 0.3825f);
+            menu.transform.position = Player.Instance.leftHandTransform.position + new Vector3(0, 0, -8f);
             menu.transform.rotation = Player.Instance.leftHandTransform.rotation;
             UnityEngine.Object.Destroy(menu.GetComponent<BoxCollider>());
-            menu.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+            menu.GetComponent<Renderer>().material.SetColor("_Color", Color.black);
             return menu;
         }
-
         public GameObject CreateButton(string modName)
         {
-            // Create the button GameObject
             GameObject btn = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            btn.transform.localScale = new Vector3(0.08f, 0.6f, 0.08f);
+            btn.transform.localScale = new Vector3(0.09f, 0.9f, 0.08f);
 
-            // Ensure the button has a Collider component and set it as a trigger
-            Collider btnColl = btn.GetComponent<Collider>();
+            BoxCollider btnColl = btn.GetComponent<BoxCollider>();
             if (btnColl != null)
             {
-                btnColl.isTrigger = true; // So OnTriggerEnter can be used
+                btnColl.isTrigger = true;
+                btnColl.size = new Vector3(0.09f, 0.9f, 0.08f);
             }
             btn.layer = 18;
-            
-            float Offset = Buttons.Count * 0.06f;
-            Vector3 btnOffset = new Vector3(-0.15f + Offset, 0, 0.03f);
+
+            float Offset = Buttons.Count * 0.1f;
+            Vector3 btnOffset = new Vector3(0.56f, 0f, 0.28f - Offset);
             btn.transform.SetParent(MenuObject.transform, false);
             btn.transform.position = MenuObject.transform.position + btnOffset;
             btn.transform.rotation = MenuObject.transform.rotation;
-            btn.GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
+            btn.GetComponent<Renderer>().material.SetColor("_Color", HexToColor("#fff700"));
 
             Button buttonComponent = btn.AddComponent<Button>();
             buttonComponent.Name = modName;
@@ -279,38 +276,44 @@ namespace Util
 
             ButtonState[buttonComponent] = false;
 
-            Vector3 textOffset = new Vector3(-0.15f + Offset + 0.03f, 0, 0.032f);
+            Vector3 textOffset = new Vector3(-0.128f + Offset + 0, 0, 0.0174f);
             GameObject textObj = new GameObject($"{modName}_Text");
             textObj.transform.SetParent(SharedCanvas.transform, false);
-            textObj.transform.transform.position = MenuObject.transform.position + textOffset;
+            textObj.transform.position = MenuObject.transform.position + textOffset;
             textObj.transform.rotation = MenuObject.transform.rotation;
 
             Text buttonText = textObj.AddComponent<Text>();
-            buttonText.text = (modName != "" || modName != null) ? modName : "N/A";
+            buttonText.text = !string.IsNullOrEmpty(modName) ? modName : "N/A";
             buttonText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-            buttonText.fontSize = 12;
-            buttonText.resizeTextForBestFit = true;
+            buttonText.fontSize = 14;
+            buttonText.resizeTextForBestFit = false;
+            buttonText.alignment = TextAnchor.MiddleCenter;
 
             RectTransform textRectTransform = textObj.GetComponent<RectTransform>();
-
             textRectTransform.anchorMin = new Vector2(0, 0);
             textRectTransform.anchorMax = new Vector2(1, 1);
             textRectTransform.pivot = new Vector2(0.5f, 0.5f);
-
             textRectTransform.position = MenuObject.transform.position + textOffset;
+
             Quaternion newRotation = btn.transform.rotation * Quaternion.Euler(180f, 90f, 90f);
             textRectTransform.rotation = newRotation;
-            textRectTransform.sizeDelta = new Vector2(0.2f, 0.03f);
-            textRectTransform.localScale = new Vector3(0.002f, 0.002f, 0.002f);
+            textRectTransform.sizeDelta = new Vector2(0.5f, 0.5f);
+            textRectTransform.localScale = new Vector3(0.004f, 0.003f, 0.004f);
+
+            ContentSizeFitter sizeFitter = textObj.AddComponent<ContentSizeFitter>();
+            sizeFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            sizeFitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+
             CreateTitle();
             return btn;
         }
+
 
         private void CreateTitle()
         {
             if (titleObject == null)
             {
-                Vector3 textOffset = new Vector3(-0.34f, 0, 0.031f);
+                Vector3 textOffset = new Vector3(-0.18857f, 0, 0.008f);
                 GameObject textObj = new GameObject($"{Title}_Text");
                 textObj.transform.SetParent(SharedCanvas.transform, false);
                 textObj.transform.transform.position = MenuObject.transform.position + textOffset;
@@ -332,8 +335,19 @@ namespace Util
                 textRectTransform.position = MenuObject.transform.position + textOffset;
                 Quaternion newRotation = MenuObject.transform.rotation * Quaternion.Euler(180f, 90f, 90f);
                 textRectTransform.rotation = newRotation;
-                textRectTransform.sizeDelta = new Vector2(0.6f, 0.1f);
-                textRectTransform.localScale = new Vector3(0.0024f, 0.0024f, 0.0024f);
+                textRectTransform.sizeDelta = new Vector2(01f, 0.1f);
+                textRectTransform.localScale = new Vector3(0.00155f, 0.00155f, 0.00130f);
+            }
+        }
+                RectTransform textRectTransform = textObj.GetComponent<RectTransform>();
+                textRectTransform.anchorMin = new Vector2(0, 0);
+                textRectTransform.anchorMax = new Vector2(1, 1);
+                textRectTransform.pivot = new Vector2(0.5f, 0.5f);
+                textRectTransform.position = MenuObject.transform.position + textOffset;
+                Quaternion newRotation = MenuObject.transform.rotation * Quaternion.Euler(180f, 90f, 90f);
+                textRectTransform.rotation = newRotation;
+                textRectTransform.sizeDelta = new Vector2(01f, 0.1f);
+                textRectTransform.localScale = new Vector3(0.00155f, 0.00155f, 0.00155f);
             }
         }
 
@@ -349,12 +363,12 @@ namespace Util
                 SharedCanvas.transform.rotation = MenuObject.transform.rotation;
             }
         }
-        
+
         public void UpdateMenuPosition(Transform hand)
         {
-            if(MenuObject != null)
+            if (MenuObject != null)
             {
-                MenuObject.transform.position =  hand.position;
+                MenuObject.transform.position = hand.position;
                 MenuObject.transform.rotation = hand.rotation;
             }
         }
@@ -380,7 +394,7 @@ namespace Util
 
         public void DisableCurrentPage()
         {
-            if(MenuObject != null)
+            if (MenuObject != null)
             {
                 MenuObject.SetActive(false);
             }
@@ -399,23 +413,31 @@ namespace Util
 
         public void ToggleModStateAndColor(string modName)
         {
-            // Find the button associated with the modName
             Button targetButton = Buttons.FirstOrDefault(b => b.modName == modName);
             if (targetButton != null)
             {
-                // Toggle the state
                 targetButton.state = !targetButton.state;
                 ButtonState[targetButton] = !ButtonState[targetButton];
-                // Get the index of the mod button to toggle the bool in the menu handler
+
                 int modIndex = MenuHandler.modNames.IndexOf(modName);
                 if (modIndex != -1)
                 {
                     MenuHandler.modStates[modIndex] = !MenuHandler.modStates[modIndex];
                 }
+
                 Color originalColor = targetButton.buttonObject.GetComponent<Renderer>().material.color;
-                Color newColor = originalColor == Color.red ? Color.grey : Color.red;
+                Color newColor = originalColor == Color.grey ? HexToColor("#2e2e2e") : Color.grey;
                 targetButton.buttonObject.GetComponent<Renderer>().material.SetColor("_Color", newColor);
             }
+        }
+
+        private Color HexToColor(string hex)
+        {
+            hex = hex.Replace("#", "");
+            byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+            byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+            byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+            return new Color32(r, g, b, 255);
         }
     }
 }
